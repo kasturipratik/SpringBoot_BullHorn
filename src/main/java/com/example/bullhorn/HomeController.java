c6f1d84008dbf2f3e02fbef9df264cb38dbde633
kasturipratik <pratik.parajuli@yahoo.com>
2018-06-27 10:39:05 -0400

    working bull horn

--- src/main/java/com/example/bullhorn/HomeController.java
+++ src/main/java/com/example/bullhorn/HomeController.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -32,7 +29,7 @@ public class HomeController {
     }
 
     @PostMapping("/process")
-    public String processTwits(@Valid BullHorn twits, BindingResult result){
+    public String processTwits(@Valid @ModelAttribute("twits") BullHorn twits, BindingResult result){
         if(result.hasErrors()){
             return "twitsForm";
         }
